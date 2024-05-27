858c493872abaca133a642b2eee05cc3cce13517
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-05-27 23:34:52 +0200

    added navbar component

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import TopNavbar from '../components/TopNavbar.vue'
 </script>
 
 <template>
   <main>
+    <TopNavbar />
     <TheWelcome />
   </main>
 </template>
